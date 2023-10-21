e4896b9a8103fb0dd0fef90bb900c805d98d9da2
kalrog-dev <16406921+kalrog-dev@users.noreply.github.com>
2023-10-21 20:11:10 +0200

    Search user by name

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ FSJS Project 2 - Data Pagination and Filtering
 // Global variables
 const itemsPerPage = 9;
 const linkList = document.querySelector(".link-list");
+let filteredList;
 
 // Build html to inject and display 9 student cards per page
 function showPage(list, page) {
@@ -35,27 +36,64 @@ function showPage(list, page) {
 
 // Build html to inject and display pagination buttons
 function addPagination(list) {
-  const numberOfPages = Math.ceil( list.length / itemsPerPage );
   linkList.innerHTML = "";
+  if (list.length === 0) return;    // No search matches
+
+  const numberOfPages = Math.ceil( list.length / itemsPerPage );
   let html = "";
   for (let i = 1; i <= numberOfPages; i++) {
     html += `<li><button type="button">${i}</button></li>`
   }
   linkList.innerHTML = html;
+
   const firstBtn = document.querySelector(".pagination button");
   firstBtn.classList.add("active");
 }
 
-// Call functions
-showPage(data, 1);
-addPagination(data);
+// Display unfiltered student list and pagination buttons
+showPageAndPagination(data);
 
-// Listener for clicks on the .link-list element
+// Listener for pagination button clicks
 linkList.addEventListener("click", (event) => {
   if (event.target.closest("button")) {
     document.querySelector(".active").classList.remove("active");
     event.target.classList.add("active");
     const pageNumber = parseInt(event.target.textContent);
-    showPage(data, pageNumber);
+    // Grab data if filteredList is undefined
+    const list = filteredList || data;
+    showPage(list, pageNumber);
   }
 });
+
+// Insert html for a search field
+const htmlSearch = 
+  `<label for="search" class="student-search">
+    <span>Search by name</span>
+    <input id="search" placeholder="Search by name...">
+    <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+  </label>`;
+const header = document.querySelector(".header");
+header.insertAdjacentHTML("beforeend", htmlSearch);
+
+// Filter student list
+const searchField = document.getElementById("search");
+searchField.addEventListener("input", getStudentsByName);
+function getStudentsByName() {
+  const input = searchField.value.toLowerCase();
+  filteredList = data.filter(student => {
+    const fullName = `${student.name.first} ${student.name.last}`.toLowerCase();
+    return fullName.includes(input) ? true : false;
+  });
+  showPageAndPagination(filteredList);
+
+  // No matches found
+  if (filteredList.length === 0) {
+    // document.querySelector(".student-list").insertAdjacentHTML("beforeend", `<p>${msg}</p>`);
+  }
+}
+
+// Display everything
+function showPageAndPagination(list) {
+  showPage(list, 1);
+  addPagination(list);
+}
